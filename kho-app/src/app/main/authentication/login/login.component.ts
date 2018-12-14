d1b8de1f4f2c80bc474153cfec8e94159cedad09
Nguyen Thanh Dat <datnt1@nal.vn>
2018-12-14 16:12:27 +0700

    [mod]

--- kho-app/src/app/main/authentication/login/login.component.ts
+++ kho-app/src/app/main/authentication/login/login.component.ts
@@ -20,7 +20,6 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LoginComponent implements OnInit {
     Pusher = require('pusher-js');
-    axios = require('axios');
 
     loginForm: FormGroup;
 
@@ -101,24 +100,28 @@ export class LoginComponent implements OnInit {
                     setTimeout(() => {
                         this.router.navigate(['/dashboard']);
                     }, 1000);
+                    this.notification(user.username);
                 },
                 () => {
                     this.loginError = true;
                 },
             );
     }
-    notificatio (): void {
+    notification (user): void {
+        console.log(user);
         const pusher = new this.Pusher('32f3c61f78d9f66b2d26', { cluster: 'ap1', forceTLS: true});
+        const axios = require('axios');
 
         // retrieve the socket ID once we're connected
         pusher.connection.bind('connected', function (): void  {
             // attach the socket ID to all outgoing Axios requests
-            this.axios.defaults.headers.common['X-Socket-Id'] = pusher.connection.socket_id;
+            axios.defaults.headers.common['X-Socket-Id'] = pusher.connection.socket_id;
             console.log('socketID: ', pusher.connection.socket_id);
         });
         Notification.requestPermission();
-        pusher.subscribe('notifications')
+        pusher.subscribe(user)
                 .bind('post_updated', function (post): void {
+                    console.log(post);
                     const notification = new Notification('trung đang gửi notification',
                     {
                         icon: 'https://cdn1.iconfinder.com/data/icons/ninja-things-1/1772/ninja-simple-512.png',
