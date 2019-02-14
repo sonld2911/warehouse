846fc889b4d08bc4bc1fb63058aaabda0b5b552d
Nguyen Bao Trung <trung.nb@deha-soft.com>
2019-02-14 13:47:49 +0700

    update data

--- kho-api/import_data/import_sp.py
+++ kho-api/import_data/import_sp.py
@@ -1,51 +1,78 @@
 import pymongo
 from xlrd import open_workbook
-
+from datetime import datetime, timedelta
 class Sp(object):
     def __init__(self):
         self.sever = "mongodb://127.0.0.1:27017/"
-        self.db_name = "products_1"
+        self.db_name = "products"
     def run(self):
         try:
             connection = pymongo.MongoClient(self.sever)
-            db = connection["kho"]
-            # db = connection[self.db_name]
-            # wb = open_workbook("./data_nal.xlsx")
-            # for sheet in wb.sheets():
-            #     number_of_rows = sheet.nrows
-            #     number_of_columns = sheet.ncols
-
-            #     items = []
-            #     for row in range(14, number_of_rows):
-            #         values = []
-            #         for col in range(number_of_columns):
-            #             value  = (sheet.cell(row,col).value)
-            #             try:
-            #                 value = str(int(value)).encode('UTF-8')
-            #             except ValueError:
-            #                 pass
-            #             finally:
-            #                 values.append(value)
-            #         # item = Arm(*values)
-            #         # print(values[1])
-            #         # items.append(item)
-            a = db.getCollection('products').find({})
-            print(a)
-            # db.insert([{
-            #     "statistical" : {
-            #         "new" : 0,
-            #         "recovery" : 0,
-            #         "guarantee" : 0
+            db = connection["ws"]
+            products_1 = db[self.db_name]
+            warehouses = db["warehouses"]
+            warehouses_01 = warehouses.find_one({"name": "WAREHOUSE 01"})
+            print(warehouses_01.get("_id"))
+            # products_1.insert([{
+            #     "statistical": {
+            #         "new": 0,
+            #         "recovery": 0,
+            #         "guarantee": 0
             #     },
-            #     "description" : values[9],
-            #     "manufacturer" : values[5],
-            #     "machinePart" : values[4],
-            #     "code" : values[0],
-            #     "name" : values[2],
-            #     "warehouseId" : ObjectId("5c0e8eab8803f91fb9465fb1"),
-            #     "createdAt" : datetime.now(),
-            #     "updatedAt" : datetime.now(),
+            #     "description": "sadasdas",
+            #     "manufacturer": "Sadasd",
+            #     "machinePart": "ASdasdsa",
+            #     "code": "Asdasdasd",
+            #     "name": "Asdasd",
+            #     "warehouseId": warehouses_01.get("_id"),
+            #     "createdAt": datetime.now(),
+            #     "updatedAt": datetime.now(),
             # }])
+            wb = open_workbook("./BaoCaoXuatNhapTon.xlsx")
+
+            sh = wb.sheet_by_index(1)
+            print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
+
+            number_of_rows = sh.nrows
+            number_of_columns = sh.ncols
+            print("{} {}".format(number_of_rows, number_of_columns))
+            for row in range(14, 3462):
+                values = []
+                for col in range(number_of_columns):
+                    value  = (sh.cell(row,col).value)
+                    try:
+                        value = str(int(value)).encode('UTF-8')
+                    except ValueError:
+                        pass
+                    finally:
+                        values.append(value)
+                # item = Arm(*values)
+                print(row)
+                # print(values)
+                data_insert = {
+                    "statistical" : {
+                        "new" : 0,
+                        "recovery" : 0,
+                        "guarantee" : 0
+                    },
+                    "technicalSpecifications": values[4],
+                    "unit": values[6],
+                    "kind": values[8],
+                    "description" : values[9],
+                    "manufacturer" : values[5],
+                    "machinePart" : values[4],
+                    "code" : values[0],
+                    "name" : values[2],
+                    "warehouseId" : warehouses_01.get("_id"),
+                    "createdAt" : datetime.now(),
+                    "updatedAt" : datetime.now(),
+                }
+                checkSP = products_1.find({"name": values[0]})
+                checkSP = list(checkSP)
+                if len(checkSP) == 0:
+                    products_1.insert([data_insert])
+                print(data_insert)
+                # products_1.insert([data_insert])
         except Exception:
             print("Can't connect mongoDB")
 Sp().run()
